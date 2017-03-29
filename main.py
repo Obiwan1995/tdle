01c0b607b9185959280c37cd1b2153027f5726d0
SandrosLaptop <alexandre.sandros@gmail.com>
2017-03-29 16:49:54 +0200

    Add reduce_wordcount Add call method with map and reduce

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import os
 
 
-def call_script_with_file(script_path, filename):
+def call_map_with_file(script_path, filename):
     if os.name == "nt":
         # Windows mode
         os.system("python " + script_path + " < data/" + filename)
@@ -14,10 +14,19 @@ def call_script_with_file(script_path, filename):
         os.system("cat data/" + filename + " | python3 " + script_path)
 
 
+def call_mapreduce_with_file(script_path_map,script_path_reduce, filename):
+    if os.name == "nt":
+        # Windows mode
+        os.system("python "+script_path_map+ " < data/" + filename+ " |  python " + script_path_reduce )
+
+    else:
+        # Other
+        os.system("cat data/" + filename + " | python3 " + script_path_map + " | python3 "+script_path_reduce)
+
 def main():
     print("Hello word")
 
-    call_script_with_file("other/map_wordcount.py", "test.txt")
+    call_mapreduce_with_file("other/map_wordcount.py","other/reduce_wordcount.py", "test.txt")
 
 
 if __name__ == '__main__':
