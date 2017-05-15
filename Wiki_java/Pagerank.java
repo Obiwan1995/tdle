4b3e3240e9d530fa33e4ba853e0a65f06cf881a7
zanaglio <a.schutzger@live.Fr>
2017-05-16 00:06:27 +0200

    Refactoring des commentaires de PageRank.java

--- Wiki_java/Pagerank.java
+++ Wiki_java/Pagerank.java
@@ -1,39 +1,32 @@
 import java.util.Arrays;
 
 
-/* 
- * Calculates the pagerank
- */
+
 final class Pagerank {
 	
-	/*---- Fields ----*/
-
-	//Store the score of the pagerank
-	private double[] scores;
-	
-	//List of page with links associated
-	// target page ID, number of incoming links, source page IDs.
-	private int[] links;
-	
-	// Maximum page ID value plus 1. This sets the length of various arrays.
 	private int idLimit;
 	
-	// Number of page IDs with incoming links or outgoing links (ignores disconnected nodes).
+	// Number of page IDs with incoming links or outgoing links
 	private int totalActive;
 	
-	// Indicates whether each page ID is active or not. Length equals idLimit.
+	//List of page with links associated Target page ID
+	private int[] links;
+
+	// Indicates whether each page ID is active or not
 	private boolean[] isActive;
 	
-	// The number of outgoing links each page ID has. Length equals idLimit.
+	//Score
+	private double[] scores;
+
+	// The number of outgoing links each page ID has
 	private int[] numOutGoingLinks;
 
-	// Indicates whether each page ID has incominglinks. Length equals idLimit.
-	private boolean[] hasIncomingLinks;
-	
-	// Temporary array, which is filled and discarded per iteration. Length equals idLimit.
+	// Temporary array
 	private double[] newScores;
 
-	// Applied damping at each iteration of the pagerank
+	// Set if each page ID has incominglinks.
+	private boolean[] hasIncomingLinks;
+
 	private final double DAMPING = 0.85;
 
 	public double[] getScores()
@@ -46,24 +39,25 @@ final class Pagerank {
 		this.scores = scores;
 	}
 	
-	// Find the highest page ID among all links
+	// Wth all the links, find the highest page ID 
 	private int findHighId ()
 	{
 		int maxId = 0;
 		int i = 0;
 		while (i < this.links.length)
 		{
-			// dest is the target page id
+			// Target page id
 			int dest = this.links[i];
-			// Compare the current id to the max
+
 			maxId = Math.max(dest, maxId);
-			// numIncoming is the number of incoming links
+
+			// Number of incoming links
 			int numIncoming = this.links[i + 1];
-			// For each incoming links from the target page id
-			// We check if it has an id over the maxId
+
+			// Check if it has an id over the maxId
 			for (int j = 0; j < numIncoming; j++)
 			{
-				//The source of an incoming link
+				//Source of an incoming link
 				int src = this.links[i + 2 + j];
 				maxId = Math.max(src, maxId);
 			}
@@ -73,26 +67,45 @@ final class Pagerank {
 		return maxId;
 	}
 
-	// Set if the target id has incoming links and set the number of outgoinglink of an id.
+	// Set wheter or not if the target id has incoming links and set the number of outgoinglink of an id.
 	private void setIncAndNumber()
 	{
 		int i = 0;
 		while (i < this.links.length)
 		{
-			// dest is the target page id
+			// Target page id
 			int dest = this.links[i];
 			this.hasIncomingLinks[dest] = true;
-			// numIncoming is the number of incoming links
+
+			// Number of incoming links
 			int numIncoming = this.links[i + 1];
+
 			// For each incoming links from the target page id
 			// We set up the number of outgoingLinks
 			for (int j = 0; j < numIncoming; j++)
 			{
-				// The source of an incoming link
+				// Incoming link
 				int src = this.links[i + 2 + j];
 				this.numOutGoingLinks[src]++;
 			}
 			i += numIncoming + 2;
+		}
+	}
+
+
+
+	// Initialize the score for all the pages
+	private void initScore()
+	{
+
+		double initWeight = 1.0 / this.totalActive;
+
+		for (int i = 0; i < this.idLimit; i++)
+		{
+			if (this.isActive[i])
+			{
+				this.scores[i] = initWeight;
+			}
 		}
 	}
 
@@ -108,26 +121,10 @@ final class Pagerank {
 			}
 		}
 	}
-
-	// Initialize the score of each page
-	private void initScore()
-	{
-
-		double initWeight = 1.0 / this.totalActive;
-
-		for (int i = 0; i < this.idLimit; i++)
-		{
-			if (this.isActive[i])
-			{
-				this.scores[i] = initWeight;
-			}
-		}
-	}
 	
-	/*---- Constructor ----*/
+	// ------------ Constructor
 	
-	// Constructs a PageRank calculator based on the given array of links
-	// in the compressed format returned by class PageLinksList.
+	// Constructs a PageRank calculator 
 	public Pagerank(int[] links)
 	{
 		// Set the links
@@ -160,7 +157,7 @@ final class Pagerank {
 	}
 	
 	
-	/*---- Methods ----*/
+	// ------------ Methods
 
 	// Divide the score by the number of outgoing links
 	private void divideByOutGoing()
@@ -180,14 +177,14 @@ final class Pagerank {
 		int i = 0;
 		while (i < this.links.length)
 		{
-			// dest is the target page id
+			// Target page id
 			int dest = this.links[i];
 			// numIncoming is the number of incoming links
 			int numIncoming = this.links[i + 1];
 			double sumPastScore = 0;
 			for (int j = 0; j < numIncoming; j++)
 			{
-				// The source of an incoming link
+				// Source of an incoming link
 				int src = this.links[i + 2 + j];
 				// Adding the past scores
 				sumPastScore += this.scores[src];
@@ -198,7 +195,7 @@ final class Pagerank {
 		}
 	}
 
-	// Generate Bias to compensate non active page
+	// Generate Bias for non active page
 	private double generateBias()
 	{
 		double bias = 0;
@@ -213,21 +210,18 @@ final class Pagerank {
 		return bias;
 	}
 
-	// Performs one iteration of the PageRank algorithm and updates the values in the array 'scores'.
+	// Compute scores
 	public void calculScore()
 	{
-		// Pre-divide by number of outgoing links
 		divideByOutGoing();
 		
-		// Distribute PageRanks over links (main calculation)
+	
 		Arrays.fill(this.newScores, 0);
 
 		setNewScore();
 		
-		// Calculate global bias due to pages without outgoing links
 		double bias = generateBias();
 		
-		// Apply bias and damping to all active pages
 		double temp = bias * this.DAMPING + (1 - this.DAMPING) / totalActive;
 
 		for (int i = 0; i < idLimit; i++)
